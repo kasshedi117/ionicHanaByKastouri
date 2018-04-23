9c0e786dd7127031812bc292954cd2eadf4a3f79
kasshedi117 <medhedhi.kestouri@esprit.tn>
2018-04-23 11:35:19 +0200

    add getData function

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient , HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,30 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController , public http : HttpClient) {
 
   }
+
+    getData(){
+
+        let headers = new HttpHeaders();
+        headers = headers.append('Content-Type', 'application/json');
+        headers = headers.append('Authorization', 'Basic youToken' );
+
+
+
+         let urlData                        : any = "yourUrl/getData.xsjs";
+
+      this.http.get(urlData ,  {headers: headers}  )
+      .subscribe((data : any) =>
+      {
+        console.log(data);
+      },
+      (error : any) =>
+      {
+         console.dir(error);
+      });
+   }
+
 
 }
